c9d5b84f0a40c654df7c90ca8baec0e1f3e5342c
joey-lott <joeylott@gmail.com>
2017-11-18 16:52:58 -0500

    more debug

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,35 @@ Route::post('/straightoutta', function (Request $request) {
 use App\RPL\TextToMarkup;
 Route::get('/test', function() {
 
-dump(phpinfo());
+  $font = "GILLUBCD.ttf";
+  $fontSize = 150;
+  $image = imagecreatetruecolor(500, 500);
+  $black = imagecolorallocate($image, 0, 0, 0);
+  $white = imagecolorallocate($image, 255, 255, 255);
+  $red = imagecolorallocate($image, 255, 0, 0);
+  $green = imagecolorallocate($image, 0, 255, 0);
+  imagefilledrectangle($image, 0, 0, 500, 500, $white);
+
+
+  $phrase = "PSYCHOTIC TEACHER";
+  $chars = preg_split("//u", $phrase, null, PREG_SPLIT_NO_EMPTY);
+
+  $x = 0;
+  $y = 200;
+  $kerning = 0;
+  foreach($chars as $char) {
+    $charResponse = imagettftext($image, $fontSize, 0, $x, $y, $black, "../fonts/".$font, $char);
+    $characterBox = imagettfbbox($fontSize, 0, "../fonts/".$font, $char);
+    var_dump($characterBox);
+    var_dump($charResponse);
+    //$characterWidth = $characterBox[2] - $characterBox[0];
+    $x = $charResponse[2] + $kerning;
+    //var_dump($charResponse[4]);
+  }
+
+
+  imagedestroy($image);
+
   // $phrase = "señor 123 456:::7890 [[[color=FF0000]]]a:::[[[/color]]][[[color=00FF00]]]bc defg hijk[[[/color]]] lmno p[[[color=0000FF]]]qrs[[[/color]]]tu";
   // $t2m = new TextToMarkup($phrase);
   // dd($t2m->words);
